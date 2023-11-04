099ad68aedf424a39b623745643b4b9b4fda4bbe
Bt Abdul <46757797+BtAbdul@users.noreply.github.com>
2023-11-04 15:51:30 +0500

    Update install.ps1
    nvim Change to Nvchad

--- installer/install.ps1
+++ installer/install.ps1
@@ -17,7 +17,7 @@ function Clone-Repository {
 
 # Main script
 $NvChadRepo = "https://github.com/NvChad/NvChad.git"
-$NvChadConfig = "$env:LOCALAPPDATA\nvim"
+$NvChadConfig = "$env:LOCALAPPDATA\NvChad"
 $ConfigRepo = "https://github.com/Alexis12119/nvim-config.git"
 
 # Check if a Neovim configuration already exists
